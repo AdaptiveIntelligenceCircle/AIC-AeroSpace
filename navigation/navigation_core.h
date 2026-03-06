24987df6af28cca18521ca0b8e9bb365fa6ebbbd
ndtribk <ndtribk@gmail.com>
2026-03-06 22:33:11 +0700

    update navigation

---
+++ navigation/navigation_core.h
@@ -0,0 +1,10 @@
+#pragma once 
+#include "trajectory_planner.h"
+#include "hazard_detector.h"
+#include "human_override_interface.h"
+
+#include "ethical_navigation_layer.h"
+
+#include "../core/mission_state.h"
+using namespace std; 
+
